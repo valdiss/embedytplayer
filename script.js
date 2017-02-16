f5e1dbc1690879878e1ba16ef6754997629c7ce5
valdiss <valentin.dissais@gmail.com>
2017-02-16 16:20:43 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+    $(".trailer").click(function(e) {
+        e.preventDefault();
+        $.fancybox({
+            'padding': 0,
+            'type': 'swf',
+            'href': this.href.replace(new RegExp("watch\\?v=", "i"), 'v/'),
+            'swf': {
+                'wmode': 'transparent',
+                'allowfullscreen': 'true'
+            }
+        });
+    });
+
+});
