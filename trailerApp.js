63ab3ab6233f45a00119095a7b0c54f8f7f3d385
valdiss <valentin.dissais@gmail.com>
2017-02-17 09:00:26 +0100

    file name changes

--- trailerApp.js
+++ trailerApp.js
@@ -1,13 +1,11 @@
-// Define the `phonecatApp` module
 let singleApp = angular.module('singleApp', []);
 
-// Define the `PhoneListController` controller on the `phonecatApp` module
 singleApp.controller('trailerController', function trailerController($scope, $http) {
-  let request = $('h1').text() + 'official trailer';
+  let request = $('h1').text() + ' trailer';
   console.log(request);
 
   $http.get('https://www.googleapis.com/youtube/v3/search?part=snippet&q='+ request +'&key=AIzaSyBo6GWJnzMlR_M4WQOQGYBcZ2lU_Lgw2LQ').then(function(response) {
-            let zipCodes = response.data;
-            $scope.videoId = zipCodes.items[0].id.videoId;
+            let filmList = response.data;
+            $scope.videoId = filmList.items[0].id.videoId;
           });
 });
